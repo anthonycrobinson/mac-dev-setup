535da658c7b3ded5d69b3b370dd87176372e9685
Anthony Robinson <arobinson@Anthonys-Mac.carolina.rr.com>
2017-11-07 20:35:09 -0500

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/env sh
+set -e
+
+touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+PROD=$(softwareupdate -l |
+  grep "\*.*Command Line" |
+  head -n 1 | awk -F"*" '{print $2}' |
+  sed -e 's/^ *//' |
+  tr -d '\n')
+softwareupdate -i "$PROD" --verbose;
+
+easy_install pip
+
+pip install ansible
+
