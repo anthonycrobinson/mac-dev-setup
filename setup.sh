8c7f3b1bcbd1b64e975abf6734347aa0ec4b1301
arobinson@example.com <arobinson@Anthonys-Mac.carolina.rr.com>
2017-11-07 20:54:57 -0500

    Add conditionals

--- setup.sh
+++ setup.sh
@@ -1,15 +1,26 @@
 #!/bin/env sh
 set -e
 
-touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
-PROD=$(softwareupdate -l |
-  grep "\*.*Command Line" |
-  head -n 1 | awk -F"*" '{print $2}' |
-  sed -e 's/^ *//' |
-  tr -d '\n')
-softwareupdate -i "$PROD" --verbose;
+scriptUser=`logname`
 
-easy_install pip
+if [[ ! `xcode-select -v` ]]; then
+  touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+  PROD=$(softwareupdate -l |
+    grep "\*.*Command Line" |
+    head -n 1 | awk -F"*" '{print $2}' |
+    sed -e 's/^ *//' |
+    tr -d '\n')
+  softwareupdate -i "$PROD" --verbose;
+fi
 
-pip install ansible
+if [[ ! `pip -V` ]]; then
+  easy_install pip
+fi
+
+if [[ ! `ansible --version` ]]; then
+  pip install ansible
+fi
+
+mkdir -p /apps
+chown $scriptUser /apps
 
